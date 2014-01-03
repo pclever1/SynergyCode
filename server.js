efe32d068ddf792a5a96753019722993a809052f
anirudh <mathukumillia@s.dcsdk12.org>
2014-01-03 06:31:55 -0700

    can chose file from menu
    next update: ability to save files

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var http = require('http');
 var fs = require('fs');
 
-var htmldata;
 var svr = http.createServer(function(req, resp) {
   fs.readFile(__dirname + '/public/index.html', function (err, data) {
     if (err) {
@@ -10,32 +9,28 @@ var svr = http.createServer(function(req, resp) {
     }
 	resp.writeHead(200, { 'Content-Type': 'text/html' });
 	resp.end(data);
-	//htmldata = data;
-	//console.log(data);
-	
-	//socket.on('send', function (data) {
-		//io.sockets.emit('message', data);
-	//});
-	fs.readFile(__dirname + '/public/create.html' , "utf8" , function(err,data){
-		if(err){
-			resp.writeHead(500);
-			return resp.end('Error loading html page');
-		}
-		htmldata = data;		
 	});
-	
-});
-	
 });
 
 var io = require('socket.io').listen(svr);
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: htmldata });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
+    socket.emit('message', { message: 'hello' });
+   // socket.on('send', function (data) {
+     //   io.sockets.emit('message', data);
+    //});
+	socket.on('fileLoad', function(data){
+		console.log(data.message);
+		fs.readFile(__dirname + '/public/' + data.message , "utf8" , function(err,data){
+			if(err){
+				resp.writeHead(500);
+				return resp.end('Error loading html page');
+			}
+			io.sockets.emit('fileData', {message: data});
+		});
+	});	
 });
+
 
 svr.listen(80, function() {
   console.log('The server is listening on port 80');
