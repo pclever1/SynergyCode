a3dd4e2f05757f4a11bf89d8e52be1f35c6421fd
pricemc <price.mc1x@gmail.com>
2014-01-07 19:35:52 -0700

    Express and Socket.io working together.
    Express and Socket.io working but without session ids

--- app.js
+++ app.js
@@ -6,9 +6,11 @@
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
+var filetest = require('./routes/filetest');
 var http = require('http');
 var path = require('path');
-
+var io = require('socket.io');
+var fs = require('fs');
 var app = express();
 
 // all environments
@@ -20,7 +22,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('youre being watched'));
 app.use(express.session());
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
@@ -33,7 +35,39 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/filetest', filetest.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+var sio = io.listen(server);
+
+var connect = require('connect');
+ 
+
+
+sio.sockets.on('connection', function (socket) {
+    console.log('A socket with sessionID ' + socket.handshake.sessionID 
+        + ' connected!');
+  socket.emit('message', { message: 'hello' });
+        socket.on('fileLoad', function(data){
+                fileName = data.message;
+                fs.readFile(__dirname + '/public/' + data.message , "utf8" , function(err,data){
+                        if(err){
+                                throw err;
+                        }
+                        socket.emit('fileData', {message: data});
+                });
+        });        
+        socket.on('fileChanged', function(data){
+                console.log('FILE CHANGED ON SERVER SIDE');
+                fs.writeFile('public/' + fileName, data.message, function(err){
+                        if(err){
+                                throw err;
+                        }
+                        console.log("THE FILE WAS SAVED");
+                });
+        });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'CodeBox' });
+	res.render('index', { title: 'CodeBox' });
 };
