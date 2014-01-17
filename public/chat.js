172ea622d339800bdd065734618cfbfca9eb855d
pricemc <price.mc1x@gmail.com>
2014-01-17 11:27:22 -0700

    strip tags

--- public/chat.js
+++ public/chat.js
@@ -13,9 +13,10 @@ window.onload = function() {
             var html = '';
             for(var i=0; i<messages.length; i++) {
                 html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-                messages[i].message.stripTags;
+
                 html += messages[i].message + '<br />';
             }
+            html.stripTags;
             content.innerHTML = html;
             $('#content').scrollTop($('#content')[0].scrollHeight);
         } else {
