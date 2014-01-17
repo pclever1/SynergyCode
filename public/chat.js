04995a7b4bcfc7bf5c088557f70267ac168e2d69
pricemc <price.mc1x@gmail.com>
2014-01-16 23:42:33 -0700

    edit

--- public/chat.js
+++ public/chat.js
@@ -1,7 +1,7 @@
 window.onload = function() {
  
     var messages = [];
-    var socket = io.connect('http://67.166.48.206:3000');
+    var socket = io.connect(location.hostname);
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
