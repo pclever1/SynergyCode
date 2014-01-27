f847750cc94649104c45c49bff42638b35bd9df0
pricemc <price.mc1x@gmail.com>
2014-01-26 20:56:48 -0700

    Logs in console better for debugging

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.cookieParser('youre being watched'));
 app.use(express.bodyParser());
-app.use(express.session());
+app.use(express.session({secret: 'secret', key: 'express.sid'}));
 app.use(app.router);
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/public')));
--- public/js/mainCode.js
+++ public/js/mainCode.js
@@ -1,74 +1,92 @@
- var socket = io.connect(location.hostname);
+//Authorization
+var socket = io.connect(location.hostname);
+socket.on('error', function (reason) {
+    console.error('Unable to connect Socket.IO', reason);
+});
 
- $(document).ready(function () {
-     $('#filetree').fileTree({
-         root: '/editableFiles',    //this line doesn't throw any errors, but it has not been confirmed to work
-         script: '/'  //the site is unable to find or use this resource
-     }, function (file) {
-         fileLoader(file);
-     });
- });
+socket.on('connect', function () {
+    console.info('successfully established a working and authorized connection');
+});
 
- $('#logout').on('click',function(){
+//filetree structure
+$(document).ready(function () {
+    $('#filetree').fileTree({
+        root: '/editableFiles', //this line doesn't throw any errors, but it has not been confirmed to work
+        script: '/' //the site is unable to find or use this resource
+    }, function (file) {
+        fileLoader(file);
+    });
+});
+
+//logout button
+$('#logout').on('click', function () {
     socket.emit('logout');
- });
+});
 
- function fileLoader(filePath) {
-     socket.emit('fileLoad', {
-         message: filePath
-     });
- }
 
- function preview(file) {
-     var html = $.parseHTML(file, document, true);
-     $('[name="preview"]').contents().find("html").html(html);
- }
 
- socket.on('fileData', function (data) {
-     editor.setValue(data.message);
-     preview(data.message);
- });
+//editor
+function fileLoader(filePath) {
+    socket.emit('fileLoad', {
+        message: filePath
+    });
+}
 
- var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
-     mode: "application/xml",
-     styleActiveLine: true,
-     lineNumbers: true,
-     lineWrapping: true,
-     theme: 'monokai'
- });
- editor.on('change', function (cMirror) {
-     var fileData = editor.getValue();
-     preview(fileData);
-     socket.emit('fileChanged', {
-         message: fileData
-     });
- });
-jQuery.fn.stripTags = function() { return this.replaceWith( this.html().replace(/<\/?[^>]+>/gi, '') ); };
+function preview(file) {
+    var html = $.parseHTML(file, document, true);
+    $('[name="preview"]').contents().find("html").html(html);
+}
+
+socket.on('fileData', function (data) {
+    editor.setValue(data.message);
+    preview(data.message);
+});
+
+var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+    mode: "application/xml",
+    styleActiveLine: true,
+    lineNumbers: true,
+    lineWrapping: true,
+    theme: 'monokai'
+});
+
+editor.on('change', function (cMirror) {
+    var fileData = editor.getValue();
+    preview(fileData);
+    socket.emit('fileChanged', {
+        message: fileData
+    });
+});
+
+$(document).ready(function () {
+    $('#editor').css("width", screen.width / 2);
+});
+
+//chat
+jQuery.fn.stripTags = function () {
+    return this.replaceWith(this.html().replace(/<\/?[^>]+>/gi, ''));
+};
 var chatOpen = false;
 
- $('#chatPulloutBar').on("click", function(e){
-    if(!chatOpen){
+$('#chatPulloutBar').on("click", function (e) {
+    if (!chatOpen) {
         $('#chatContainer').animate({
-            "left":"-=20%"
+            "left": "-=20%"
         }, "slow");
         $('#chatPulloutBar').animate({
             "left": "-=20%"
-        },"slow");
+        }, "slow");
         $('#chatPulloutBar img').attr("src", "images/rightArrow.png");
         chatOpen = true;
-    }else{
+    } else {
         $('#chatContainer').animate({
-            "left":"+=20%"
+            "left": "+=20%"
         }, "slow");
         $('#chatPulloutBar').animate({
             "left": "+=20%"
-        },"slow");
+        }, "slow");
         $('#chatPulloutBar img').attr("src", "images/leftArrow.png");
         chatOpen = false;
-    }    
+    }
     e.stopPropagation();
- });
-
-$(document).ready(function(){
-     $('#editor').css("width", screen.width/2);
 });
